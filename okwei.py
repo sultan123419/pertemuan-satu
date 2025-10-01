3af8f82690ac543bc7a8cc4166f6f2cd273627bf
sultan aja <sultanahmedghj6@gmail.com>
2025-10-01 11:15:17 +0700

    Add files via upload

---
+++ okwei.py
@@ -0,0 +1,8 @@
+nama_depan = "Sultan"
+nama_belakang= 'Sultan'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '18'
+alamat = 'bangka'
+kata_mutiara = "Don't judge a book by it's cover"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ' kata mutiara:', kata_mutiara)
