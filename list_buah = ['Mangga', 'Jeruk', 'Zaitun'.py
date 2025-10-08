38cb4b7938feda816b23dcd4a94857ac8a0ee5e8
sultan aja <sultanahmedghj6@gmail.com>
2025-10-08 11:20:32 +0700

    Add files via upload

---
+++ list_buah = ['Mangga', 'Jeruk', 'Zaitun'.py
@@ -0,0 +1,8 @@
+list_buah = ['Mangga', 'Jeruk', 'Zaitun', 'Apel', 'Durian']
+print(list_buah)\
+#urutkan secara escending
+list_buah.sort()
+print(list_buah)
+# membalikkan posisi item list(tidak haryus berurut)
+list_buah.reverse()
+print(list_buah)
