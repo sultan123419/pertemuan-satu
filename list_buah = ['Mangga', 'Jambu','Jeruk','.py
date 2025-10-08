300777872e07a98c6c9e75f0639ecea69574d811
sultan aja <sultanahmedghj6@gmail.com>
2025-10-08 11:19:51 +0700

    Add files via upload

---
+++ list_buah = ['Mangga', 'Jambu','Jeruk','.py
@@ -0,0 +1,10 @@
+list_buah = ['Mangga', 'Jambu','Jeruk','Jambu']
+print(list_buah)
+
+# hapus item pertama dengan nilai 'jambu'
+list_buah.remove('Jambu')
+print(list_buah)
+del list_buah[1]
+print(list_buah)
+del list_buah[0:2]
+print(list_buah)
