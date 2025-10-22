2ea3ad1ff199ebafecdf23a23ab076b6451080c8
sultan aja <sultanahmedghj6@gmail.com>
2025-10-22 10:44:44 +0700

    Add files via upload

---
+++ membuat input phyton.py
@@ -0,0 +1,5 @@
+nama=input('Masukkan nama anda:')
+print('Halo,', nama, '.. selamat datang!')
+
+nama = input('Masukkan nama anda:')
+print('variabel nama bertipe data:', type(nama))
---
+++ perusahaan = 'microsoft'.py
@@ -0,0 +1,25 @@
+perusahaan = 'microsoft'
+list_pulau = ['Jawa', 'Sumatra', 'Sulawesi']
+
+# ini adalah dictionary, insyaallah akan kita pelajari
+#di pertemuan-pertemuan yang ajan datang
+mahasiswa = {
+    'nama': 'Lendis Fabri',
+    'asal':'Lamongan'
+}
+print(
+    "Apakah 'c' ada di variabel perusahaan?",
+    'c' in perusahaan
+)
+print(
+    "apakah 'z' tidak ada di variabel perusahaan?",
+    'c' not in perusahaan
+)
+print(
+    "apakah 'Madura' tidak ada di variabel list_pulau?",
+    'Madura' not in perusahaan
+)
+print(
+    "Apakah atribut 'nama' ada di variabel mahasiswa?",
+    'nama' in mahasiswa
+)
